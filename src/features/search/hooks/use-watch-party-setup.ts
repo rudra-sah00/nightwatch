3a99956a6a655babe9f1dcfb4178cb6af082db6f
rudra-sah00 <rudranarayanaknr@gmail.com>
2026-04-13 03:44:01 +0530

    chore: safely remove unused files and dependencies

--- src/features/search/hooks/use-watch-party-setup.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useState } from 'react';
-
-export function useWatchPartySetup() {
-  const [isSeasonOpen, setIsSeasonOpen] = useState(false);
-  return { isSeasonOpen, setIsSeasonOpen };
-}
