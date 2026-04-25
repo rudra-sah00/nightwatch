5a3c9a03c20007b581a4a5cc2e139e8b84d5463d
rudra-sah00 <rudranarayanaknr@gmail.com>
2026-04-25 13:21:28 +0530

    fix: progress bar position offset for electron title bar

--- src/components/layout/progress-bar.tsx
+++ src/components/layout/progress-bar.tsx
@@ -52,7 +52,7 @@ export function ProgressBar() {
         background: var(--neo-yellow);
         position: fixed;
         z-index: 99999;
-        top: 77px;
+        top: calc(77px + var(--electron-titlebar-height, 0px));
         left: 0;
         width: 100%;
         height: 3px;
