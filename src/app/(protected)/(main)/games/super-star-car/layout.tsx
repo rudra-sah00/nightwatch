fb7129ebb05dc139e2fb209e52890305e5b9bdb0
rudra-sah00 <rudranarayanaknr@gmail.com>
2026-05-12 18:06:51 +0530

    revert(games): remove super-star-car

--- src/app/(protected)/(main)/games/super-star-car/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = { title: 'Super Star Car' };
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
